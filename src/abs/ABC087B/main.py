7473c77ce20566b37656c69e3d6153eccbb849f1
be.with <be.with.9078182@gmail.com>
2021-05-07 09:56:14 +0900

    ABC087B

--- src/abs/ABC087B/main.py
+++ src/abs/ABC087B/main.py
@@ -3,6 +3,13 @@ import sys
 
 
 def solve(A: int, B: int, C: int, X: int):
+    ret = 0
+    for i in range(A+1):
+        for j in range(B+1):
+            for k in range(C+1):
+                if(i * 500 + j * 100 + k * 50 == X):
+                    ret += 1
+    print(ret)
     return
 
 
